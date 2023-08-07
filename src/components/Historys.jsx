fcd9f0999761e1cee2afa217a824429b71242f5b
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-08-07 15:55:06 -0300

    add history

--- src/components/Historys.jsx
+++ src/components/Historys.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './styles/historys.css'
 
 const Historys = () => {
   return (
-    <div><img src="/_nuxt/images/image-2x-460b4c.webp" alt="" /></div>
+    <div className='historys-container'>
+      <h1 className='h1-historys'>Centros de datos al rededor del mundo</h1>
+      <p className="p-historys">Nuestros planes de hosting web WordPress y cloud hosting ofrecen ubicaciones de servidor en: EE. UU., Reino Unido, Francia, India, Singapur, Brasil, Lituania y Países Bajos.</p>
+      <img className='img-historys' src="./public/img_historys.webp" alt="" /></div>
   )
 }
 
